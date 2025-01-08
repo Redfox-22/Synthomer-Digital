f7d8633d74847628e2ef0f7a4de136508d9c4137
Redfox-22 <danny.vanderhaeghe@gmail.com>
2025-01-08 19:54:40 +0000

    Added meetwaarden + API JSON

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+MEETWAARDEN = [
+  {
+    'id': 1,
+    'omschrijving': 'P-7301 A/B',
+    'waarde': '3 bar',
+  },
+  {
+    'id': 2,
+    'omschrijving': 'P-7302 A/B',
+    'waarde': '2,5 bar',
+  },
+  {
+    'id': 3,
+    'omschrijving': 'P-7303 A/B',
+    'waarde': '3,5 bar',
+  },
+]
+
+@app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                        meetwaarden=MEETWAARDEN)
+@app.route("/api/meetwaarden")
+def list_meetwaarden():
+  return jsonify(MEETWAARDEN)
 
 print(__name__)
 if __name__ == "__main__":
